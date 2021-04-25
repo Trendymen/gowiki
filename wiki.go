b0e57df2dbb73eb599ca82cd5bc8b334c6b61a87
Trendymen <476212964@qq.com>
2021-04-25 18:58:54 +0800

    Initial commit

---
+++ wiki.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func main() {
+
+}
+
+func (p Page) save() error  {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename,p.Body,777)
+}
