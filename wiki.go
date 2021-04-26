8a960ea12bfb7c322e0b584223f2bbfcaf879e29
Trendymen <476212964@qq.com>
2021-04-27 00:49:53 +0800

    .

--- wiki.go
+++ wiki.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
+	"net/http"
 )
 
 type Page struct {
@@ -11,10 +13,27 @@ type Page struct {
 }
 
 func main() {
-
+	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+	p1.save()
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func (p Page) save() error  {
+func handler(w http.ResponseWriter, r *http.Request) {
+	p2, _ := loadPage("TestPage")
+	fmt.Fprintf(w, string(p2.Body))
+}
+
+func (p Page) save() error {
 	filename := p.Title + ".txt"
-	return ioutil.WriteFile(filename,p.Body,777)
+	return ioutil.WriteFile(filename, p.Body, 777)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
 }
